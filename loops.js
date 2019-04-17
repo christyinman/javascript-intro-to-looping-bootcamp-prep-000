17b17fdf87d8bb3a0f9f2749998009425b41c323
Christy Inman <christyinman@ymail.com>
2019-04-17 00:50:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(i){
   if (1)
     return ("I am 1 strange loop.");
-  else if for(let i=0; i<25; i++)
+  else if (2-25)for(let i=0; i<25; i++)
     console.log("I am ${i} strange loops")
 }
