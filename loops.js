9e023890783ec352b3655eaf6577e8983557e183
Christy Inman <christyinman@ymail.com>
2019-04-17 00:56:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   if (1)
     return ("I am 1 strange loop.");
-  else for(let i=0; i<25; i++)
+  else for(let i===0; i<25; i++)
     console.log("I am ${i} strange loops.")
 }
